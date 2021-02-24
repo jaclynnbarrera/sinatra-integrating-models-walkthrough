07c46945e326b73b3d46eb67d294eb507754d6c3
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-02-24 18:40:20 -0500

    commit tree 194340a414c8639ff2ac7e3001573ea9f1361fe2

--- app.rb
+++ app.rb
@@ -2,13 +2,22 @@ require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
 
 class App < Sinatra::Base
+  #renders the index.erb page
   get '/' do
     erb :index
   end
 
+  #receives the form data through params and renders to results page
   post '/' do
-    text_from_user = params[:user_text]
 
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+    @number_of_words = @analyzed_text.count_of_words
+    @vowels = @analyzed_text.count_of_vowels
+    @consonants = @analyzed_text.count_of_consonants
+    @analyzed_text.most_used_letter.each do |k,v|
+      @most_common_letter = k.capitalize
+      @common_letter_count = v
+    end     
     erb :results
   end
 end
